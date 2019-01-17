9bfc321ff81ea35ba7e415fabc5dd370e95a5aab
Rodrigo F Martins <rodrigo.fonsecamartins@gmail.com>
2019-01-17 09:54:53 -0200

    Adicionado slider

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Exscrollview from './exscrollview';
-import Exflatlist from './exflatlist';
+import ExSlider from './exslider';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        {/* <Exscrollview/>  */}
-        <Exflatlist/>       
+        <ExSlider/>  
       </View>
     );
   }
